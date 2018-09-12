2e4084a218792f08ef1418aa63252ffd5ceb6de3
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2018-09-12 15:48:47 -0600

    closed issue #41 to fix npm test script

--- client/components/SingleProduct.spec.js
+++ client/components/SingleProduct.spec.js
@@ -9,7 +9,7 @@ import {SingleProduct} from './SingleProduct'
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-describe.only('SingleProduct', () => {
+describe('SingleProduct', () => {
   let fakePage
 
   beforeEach(() => {
