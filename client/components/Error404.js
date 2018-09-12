aae00d7e02d1dde84ef208fa1e451f01ff0d86d2
John Riccardi <riccjohn@gmail.com>
2018-09-12 14:14:03 -0500

    Completed 404 structure - Closes #23

--- client/components/Error404.js
+++ client/components/Error404.js
@@ -5,6 +5,7 @@ const Error404 = () => {
     <React.Fragment>
       <h2>404</h2>
       <h1>Error: Pogo not found</h1>
+      {/* automatically fetches from public folder*/}
       <img width="400" src="404image.jpg" />
     </React.Fragment>
   );
