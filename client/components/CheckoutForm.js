8ec758666fdb1dd140407551dba854ea25627e8d
John Riccardi <riccjohn@gmail.com>
2018-09-14 18:24:00 -0500

    Working on sending data to back end to create token

--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -27,6 +27,16 @@ class CheckoutForm extends Component {
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
+        <form>
+          <label>First Name</label>
+          <input type='text' name='firstName'/>
+          <label>Last Name</label>
+          <input type='text' name='lastName'/>
+          <label>Address</label>
+          <input type='text' name='address'/>
+          <label>Country</label>
+          <input type='text' name='country'/>
+        </form>
         <CardElement />
         <button type='submit' onClick={this.submit}>Send</button>
       </div>
