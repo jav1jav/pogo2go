5b0f30ed1693cfc2729b8624fb1256bd79f615d6
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2018-09-17 12:30:02 -0600

    Closes #11 - Added functionality to add items to cart, links to checkout

--- client/components/ShoppingCart.js
+++ client/components/ShoppingCart.js
@@ -3,60 +3,74 @@ import {connect} from 'react-redux'
 import {fetchUserData} from '../store/userReducer'
 import {Link} from 'react-router-dom'
 
-const isLoggedIn = false //TODO: set the login status
-
 class ShoppingCart extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props)
     this.state = {
       productList: []
-    };
+    }
   }
 
   async componentDidMount() {
+    console.log('CDM')
+    console.log('this.props', this.props)
+    let {user, isLoggedIn} = this.props
+
     if (isLoggedIn) {
-      const userId = 1 //TODO: find the user data off the session
+      const userId = user.id
       await this.props.fetchUserData(userId)
       const aUser = this.props.user
-      this.setState( aUser.orders.find(order => !order.isPurchased).products || [] )
-     } else {
-       console.log('SHOPPINGCART.JS | CDM | this.state', this.state)
-       const plist = JSON.parse(window.localStorage.getItem('productList'))
-      this.setState( {productList : plist} )
-      console.log('SHOPPINGCART.JS | CDM | this.state after setState', this.state, 'plist', plist)
-     }
+      this.setState(
+        aUser.orders.length
+          ? aUser.orders.find(order => !order.isPurchased).products
+          : []
+      )
+    } else {
+      const productListOnLocalStorage = JSON.parse(
+        window.localStorage.getItem('productList')
+      )
+      this.setState({
+        productList: productListOnLocalStorage ? productListOnLocalStorage : []
+      })
+    }
   }
 
   render() {
-
-    let productList = this.state.productList;
-    console.log('shoppingCart.js | render | productList from state', productList)
+    console.log('render')
+    console.log('this.props', this.props)
+    let {user, isLoggedIn} = this.props
+    let productList = this.state.productList
 
     // assuming that if you're logged in we're pulling from state (I think there are corner cases that will be screwed) then
 
+    const orderTotal = productList.length
+      ? productList
+          .map(product => Number(product.price))
+          .reduce((curr, acc) => curr + acc, 0)
+      : 0
 
-    const orderTotal = productList
-    .map(product => Number(product.price))
-    .reduce((curr, acc) => curr + acc, 0)
-
-    const removeItem = (removedItemId) => {
+    const removeItem = removedItemId => {
       if (isLoggedIn) {
         //dispatch thunk to remove item from order
       } else {
         //update product list in localStorage
         let cartList = JSON.parse(window.localStorage.getItem('productList'))
-        cartList.splice(cartList.findIndex(item => item.id === removedItemId), 1)
+        cartList.splice(
+          cartList.findIndex(item => item.id === removedItemId),
+          1
+        )
         console.log('cartList', cartList)
-        this.setState({productList : cartList})
+        this.setState({productList: cartList})
         window.localStorage.setItem('productList', JSON.stringify(cartList))
       }
     }
 
-// if you're logged in but you don't have a user, you have to show loading till we get the user from the update of redux state in componentDidMount
-// or
-// if you're not loggged in and there's no productList on local storage we have to render a blank cart
-//    if (isLoggedIn && aUser)  {
-    if (false) { //currently forcing the else condition while testing use of localStorage
+    // if you're logged in but you don't have a user, you have to show loading till we get the user from the update of redux state in componentDidMount
+    // or
+    // if you're not loggged in and there's no productList on local storage we have to render a blank cart
+    //    if (isLoggedIn && aUser)  {
+    if (false) {
+      //currently forcing the else condition while testing use of localStorage
       return <div>Loading!</div>
     } else {
       return (
@@ -73,7 +87,14 @@ class ShoppingCart extends Component {
                     <tr key={prod.id}>
                       <td>{prod.name}</td>
                       <td>{prod.price}</td>
-                      <td><button type='submit' onClick={() => removeItem(prod.id)}>remove item</button></td>
+                      <td>
+                        <button
+                          type="submit"
+                          onClick={() => removeItem(prod.id)}
+                        >
+                          remove item
+                        </button>
+                      </td>
                     </tr>
                   )
                 })}
@@ -86,7 +107,11 @@ class ShoppingCart extends Component {
               </tbody>
             </table>
           </div>
-          <div><Link to="/store">Continue Shopping</Link></div>
+          <div>
+            <Link to="/store">Continue Shopping</Link>
+            <br />
+            <Link to="/checkout">Checkout</Link>
+          </div>
         </React.Fragment>
       )
     }
@@ -94,7 +119,8 @@ class ShoppingCart extends Component {
 }
 
 const mapStateToProps = state => ({
-  user: state.user
+  user: state.user,
+  isLoggedIn: !!state.user.id
 })
 
 const mapDispatchToProps = dispatch => ({
